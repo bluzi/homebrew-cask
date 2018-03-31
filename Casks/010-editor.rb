7a853320ef13c81b2823e96962e0f0b419c02d72
Eliran Pe'er <eliran013@gmail.com>
2018-03-31 23:27:59 +0300

    Update 010-editor.rb

--- Casks/010-editor.rb
+++ Casks/010-editor.rb
@@ -4,7 +4,8 @@ cask '010-editor' do
   
   eliran p
 
-  url "https://download.sweetscape.com/010EditorMac64Installer#{version.no_dots}.dmg"
+  url "https://download.sweetscape.com
+/010EditorMac64Installer#{version.no_dots}.dmg"
   appcast 'https:// www.sweetscape.com/download/010editor/',
           checkpoint: 'ace07a913b064266e59ff311a5474c64585955e201c975475e8372152ca7f8a7'
   name '010 Editor'
